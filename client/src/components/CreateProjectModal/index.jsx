a506af85aecda8710847f8a3bf54bdec6f6f2e59
karanveer <karnveersharma1994@gmail.com>
2021-02-07 18:12:48 +0530

    Added validation from front-end

--- client/src/components/CreateProjectModal/index.jsx
+++ client/src/components/CreateProjectModal/index.jsx
@@ -9,20 +9,55 @@ import Row from 'react-bootstrap/Row';
 
 function CreateProjectModal(props) {
   const [title, setTitle] = useState("");
+  const [titleError, setTitleError] = useState("");
   const [description, setDescription] = useState("");
+  const [descriptionError, setDescriptionError] = useState("");
   const [deliverDate, setDeliveryDate] = useState("");
   const [projectCost, setProjectCost] = useState("");
+  const [projectCostError, setProjectCostError] = useState("");
+  const [cleverPer, setcleverPer] = useState("0.0");
+  const [total, setTotal] = useState("0.0");
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    const DataToBeSended = {
-      name: title,
-      summary: description,
-      date: deliverDate,
-      cost: projectCost, 
+    const titleCheck = title.length > 80;
+    const descriptionCheck = description.length > 100;
+    const projectCostCheck = !(100 <= projectCost ||  projectCost >= 100000);
+
+    if(titleCheck){
+      setTitleError('max length exceed from 80')
     }
-    props.postProjectData(DataToBeSended);
+    if(descriptionCheck) {
+      setDescriptionError('max length exceed from 100')
+    }
+    if(projectCostCheck){
+      setProjectCostError('project cost have to be in range 100-100000')
+    }
+
+    if(!(titleCheck || descriptionCheck || projectCostCheck)) {
+      setTitleError('');
+      setDescriptionError('');
+      setProjectCostError('');
+
+      const DataToBeSended = {
+        name: title,
+        summary: description,
+        date: deliverDate,
+        cost: projectCost, 
+      }
+      props.postProjectData(DataToBeSended);
+    }
   }
+
+  const onChangeCost = (e) => {
+    const cost = Number(e.target.value);
+    setProjectCost(cost);
+    const per = (0.2 * cost).toFixed(2);
+    setcleverPer(per);
+    const totalCost = Number(per) + cost
+    setTotal(totalCost);
+  }
+
   return (
     <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
       <Modal.Header closeButton>
@@ -33,13 +68,16 @@ function CreateProjectModal(props) {
       <Modal.Body className="show-grid">
         <Form onSubmit={handleSubmit} role="form">
         <Form.Group>
-            <Form.Control type="text" name="name" onChange={e => setTitle(e.target.value)} placeholder="Title" />
+            <Form.Control type="text" name="name" onChange={e => setTitle(e.target.value)} placeholder="Title" required />
+            {titleError.length > 0 && <span className="error">{titleError}</span>}
             <br />
-            <Form.Control as="textarea" name="summary" type="text" onChange={e => setDescription(e.target.value)} placeholder="Description" />
+            <Form.Control as="textarea" name="summary" type="text" onChange={e => setDescription(e.target.value)} placeholder="Description" required/>
+            {descriptionError.length > 0 && <span className="error">{descriptionError}</span>}
             <br />
-            <Form.Control type="date" name="date" onChange={e => setDeliveryDate(e.target.value)} placeholder="Deliver date" />
+            <Form.Control type="date" name="date" startDate={new Date()} onChange={e => setDeliveryDate(e.target.value)} placeholder="Deliver date" required />
             <br />
-            <Form.Control type="text" name="cost" onChange={e => setProjectCost(e.target.value)} placeholder="Project cost" />
+            <Form.Control type="number" name="cost" onChange={onChangeCost} placeholder="Project cost" required/>
+            {projectCostError.length > 0 && <span className="error">{projectCostError}</span>}
             <br />
             <Row>
               <Col xs={7}>
@@ -48,7 +86,7 @@ function CreateProjectModal(props) {
                 </Form.Label>
               </Col>
               <Col sm={3}>
-                <Form.Control disabled type="transFees" placeholder="$0.0" />
+                <Form.Control disabled type="transFees" placeholder={`$${cleverPer}`} />
               </Col>
               <Col sm={2}>
               </Col>
@@ -61,14 +99,15 @@ function CreateProjectModal(props) {
                 </Form.Label>
               </Col>
               <Col sm={3}>
-                <Form.Control disabled type="totalAmount" placeholder="$0.0" />
+                <Form.Control disabled type="totalAmount" placeholder={`$${total}`} />
               </Col>
               <Col sm={2}>
               </Col>
             </Row>
         </Form.Group>
+        <br/>
         <Form.Group as={Row}>
-          <Col sm={{ span: 10, offset: 2 }}>
+          <Col sm={{ span: 10, offset: 4 }}>
             <Button type="submit">Create Project</Button>
           </Col>
         </Form.Group>
